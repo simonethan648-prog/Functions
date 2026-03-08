fb668ccaa02c8eb6e17e9cc65eae033c42a56d4c
simonethan648-prog <simonethan648@gmail.com>
2026-03-08 10:49:14 +0400

    Add files via upload
    Write a Python program that takes any word as an input from the user and then creates a function that accepts the same word as a parameter and checks whether it is a palindrome or not. (Palindrome word is the same when read in both directions - forward or backward. Example - racecar)

---
+++ Main2.py
@@ -0,0 +1,6 @@
+def absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        print("Absolute Value of 89 :", absolute_value(89))
+        print("Absolute Value of -189 :", absolute_value(-189))
